45e70857563345aa9d2c07a15d66e8cda8acafb6
Claudio Pasinelli <claudiopasinelli@gmail.com>
2024-07-16 14:20:18 +0200

    fix: removed unused imports in Navbar

--- src/ui/molecules/Navbar/index.tsx
+++ src/ui/molecules/Navbar/index.tsx
@@ -1,13 +1,4 @@
-import { Link, useLocation, useParams } from 'react-router-dom';
-import { cn } from '../../../utils/helpers/tailwindMerge';
-import { ROUTES } from '../../../utils';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-// import { A } from '../../atoms';
-
 const Navbar = () => {
-  const location = useLocation();
-
   return (
     <nav className="sticky top-0 z-50 flex h-20 min-h-28 w-full items-center justify-between bg-yellow-200 px-4 py-2 shadow-xl">
       <section className="flex w-full items-center justify-start">
